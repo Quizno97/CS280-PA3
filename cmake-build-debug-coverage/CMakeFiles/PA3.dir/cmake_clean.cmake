a90add0048ac26c0eff8cb4481a60ce1cc91c474
tyrrell <tyrrellr50@gmail.com>
2020-12-24 11:33:23 -0500

    Initial commit

---
+++ cmake-build-debug-coverage/CMakeFiles/PA3.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PA3.dir/lexAnalyzer.cpp.obj"
+  "CMakeFiles/PA3.dir/main.cpp.obj"
+  "PA3.exe"
+  "PA3.exe.manifest"
+  "PA3.pdb"
+  "libPA3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PA3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
